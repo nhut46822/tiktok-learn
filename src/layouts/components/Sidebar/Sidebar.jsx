60ec6f1a59a252477dda300a869b601654962fce
ThanhNhut <nhut456822@donga.edu.vn>
2022-08-24 21:01:36 +0700

    end #27

---
+++ src/layouts/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
+
+import styles from './Sidebar.module.scss';
+import config from '~/config';
+import Menu, { MenuItem } from './Menu';
+import {
+    HomeIcon,
+    HomeActiveIcon,
+    UserGroupIcon,
+    UserGroupActiveIcon,
+    LiveIcon,
+    LiveActiveIcon,
+} from '~/components/Icons';
+
+const cx = classNames.bind(styles);
+
+function Sidebar(props) {
+    return (
+        <aside className={cx('wrapper')}>
+            <Menu>
+                <MenuItem title="For You" to={config.routes.home} icon={<HomeIcon />} activeIcon={<HomeActiveIcon />} />
+                <MenuItem
+                    title="Following"
+                    to={config.routes.following}
+                    icon={<UserGroupIcon />}
+                    activeIcon={<UserGroupActiveIcon />}
+                />
+                <MenuItem title="LIVE" to={config.routes.live} icon={<LiveIcon />} activeIcon={<LiveActiveIcon />} />
+            </Menu>
+        </aside>
+    );
+}
+
+Sidebar.propTypes = {};
+
+export default Sidebar;
---
+++ src/pages/Live/Live.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Live(props) {
+    return <h1>Live Page</h1>;
+}
+
+Live.propTypes = {};
+
+export default Live;
